51410aeb5200ff040ed4bd7d733d47d0e08a2675
rakesh3626 <159223739+rakesh3626@users.noreply.github.com>
2024-04-08 12:58:32 +0530

    Update LoginPage.java

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -22,21 +22,6 @@ public class LoginPage extends BasePage {
 	@FindBy(xpath="//span[normalize-space()='Submit']")
 	WebElement submit;
 	
-	@FindBy(xpath="//a[@href='/banking/self/payment']//div[@class='quick-access-icon']//icon//*[name()='svg']")
-	WebElement payuser;
-	
-	@FindBy(xpath="//input[@placeholder='Type to search']")
-	WebElement search;
-	
-	@FindBy(xpath="//input[@id='id_3']")
-	WebElement amount;
-	
-	@FindBy(xpath="//button[@class='btn d-flex justify-content-center align-items-center w-100 h-100 btn-primary']")
-	WebElement next;
-	
-	@FindBy(xpath="//span[normalize-space()='Confirm']")
-	WebElement confirm;
-	
 	
 	public void click_login() {
 		login.click();
@@ -49,20 +34,5 @@ public class LoginPage extends BasePage {
 	}
 	public void click_submit() {
 		submit.click();
-	}
-	public void click_payuser() {
-		payuser.click();
-	}
-	public void setsearchvalue(String contactname) {
-		search.sendKeys(contactname);	
-	}
-	public void setamount(String amnt) {
-		amount.sendKeys(amnt);
-	}
-	public void click_next() {
-		next.click();
-	}
-	public void click_confirm() {
-		confirm.click();
 	}
 }
